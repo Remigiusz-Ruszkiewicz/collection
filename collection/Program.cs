0278712383b01430299a02b04e943a8f117d0646
Remigiusz Ruszkiewicz <r.ruszkiewicz@microsoft.wsei.edu.pl>
2019-09-19 18:08:32 +0200

    Add project files.

---
+++ collection/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] names = { {"Rafał", "Anna","kasia" },{ "Rafał", "Anna","joanna" } };
+            //for (int i = 0; i < names.Length; i++)
+            //{
+            //    for (int y = 0; y < names[i].Length; y++)
+            //    {
+            //        Console.WriteLine(names[i,y]);
+            //    }
+            //}
+            for (int i = 0; i < names.GetLength(0); i++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    Console.Write(names[i,y]);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
